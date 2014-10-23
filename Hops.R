6dfed4bf0451a28909e4c9f3ba1b5ce773d1bb08
Brewstarke <afstarke@gmail.com>
2014-10-23 19:41:15 -0400

    Add webpages of interest. Hopunion and Beer legends seem to be leading the way.

--- Hops.R
+++ Hops.R
@@ -2,8 +2,18 @@ library(XML)
 
 # Pull data from BeerLegends.com
 # 
-
+# Find the best resources on the web....
 beerlegends <- "http://beerlegends.com/hops-varieties"
-tables <- readHTMLList(beerlegends)
-hoplist <- unlist(list(tables[2:4]))
-hoplist
+hopunion <- "http://www.hopunion.com/hop-varieties/"
+
+# Get links to each hop for each site- Parsing from reahHTMLList has issues.
+beerlegendsLinks <- getHTMLLinks(beerlegends)
+hop_links_BL.com <- beerlegendsLinks[9:219]
+
+hopunionLinks <- getHTMLLinks(hopunion)
+hop_links_HU.com <- hopunionLinks[38:159]
+
+# create a list of links to pull data from for each hop
+hoplisturls <-  paste("http://beerlegends.com", hop_links_BL.com, "-hops", sep="")
+hoplisturls # Should contain the urls to all the hops in beerlegends database. 
+
